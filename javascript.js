fa7d4e2fecc8aa98dc64b8a6b81e0a458261a4aa
cornertenant <135449310+cornertenant@users.noreply.github.com>
2023-06-26 11:22:09 +0200

    Add basic rock paper scissors script Very basic, should be cleaned up before moving on. Lots of repeat code, but the logic, score keeping works.

--- javascript.js
+++ javascript.js
@@ -1,20 +1,92 @@
+let score = 0;
+
+// random choice for coomputer //
 function getComputerChoice(){
 
-    let choice = "computer's choice"
+    let computerChoice = "computer's choice"
     let randomNumber = Math.floor(Math.random() * 3 + 1);
 
     if (randomNumber === 1) {
-        choice = "Rock";
-        return choice;
+        computerChoice = "rock";
+        return computerChoice;
     }
     else if (randomNumber === 2) {
-        choice = "Paper";
-        return choice;
+        computerChoice = "paper";
+        return computerChoice;
     }
     else
-        choice = "Scissors";
-        return choice;
+        computerChoice = "scissors";
+        return computerChoice;
 }
 
-getComputerChoice();
-console.log(getComputerChoice())
+// choice of the player //
+function getPlayerChoice(){
+    let playerChoice = prompt("rock, paper or scissors?");
+    playerChoice = playerChoice.toLowerCase();
+
+    if (playerChoice === "rock"){
+        return playerChoice;
+    }
+    else if (playerChoice === "paper"){
+        return playerChoice;
+    }
+    else if (playerChoice === "scissors"){
+        return playerChoice;
+    }
+    else
+        return alert("UNACCEPTABLE!");
+
+}
+
+// winning message and updateing score //
+function win(winner, loser){
+    console.log(`${winner} beats ${loser}. You win!`)
+    score++;
+}
+
+// losing message and updating score //
+function lose(loser, winner){
+    console.log(`${loser} is beaten by ${winner}. You lose!`)
+    score--;
+}
+
+// play a single round //
+function playRound(player, computer){
+    if (player == "rock" && computer == "scissors"){
+        win(player, computer);
+    }
+    else if (player == "paper" && computer == "rock"){
+        win(player, computer);
+    }
+    else if (player == "scissors" && computer == "rock"){
+        win(player, computer);
+    }
+
+    else if (player == "rock" && computer == "paper"){
+        lose(player, computer);
+    }
+    else if (player == "paper" && computer == "scissors"){
+        lose(player, computer);
+    }
+    else if (player == "scissors" && computer == "rock"){
+        lose(player, computer);
+    }
+
+    else if (player == computer){
+        return alert("TIE!");
+    }
+
+    else
+        return;
+}
+
+// play the game for 5 rounds in total //
+function playGame(){
+    
+    for (let i = 0; i < 5; i++){
+        playRound(getPlayerChoice(), getComputerChoice());
+        console.log(score);
+    }
+}
+
+playGame();
